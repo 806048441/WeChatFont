6ad9a765c713de6dacee0759db7c6a3b8f6ad94c
806048441 <806048441@qq.com>
2019-08-04 15:53:11 +0800

    1
    1

---
+++ WeChatFont/WeChatFont/b/BaseNavigationController.m
@@ -0,0 +1,31 @@
+//
+//  BaseNavigationController.m
+//  ClassMoudle
+//
+//  Created by 神州第一坑 on 2019/7/28.
+//
+
+#import "BaseNavigationController.h"
+
+@interface BaseNavigationController ()
+
+@end
+
+@implementation BaseNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ WeChatFont/WeChatFontTests/WeChatFontTests.m
@@ -0,0 +1,37 @@
+//
+//  WeChatFontTests.m
+//  WeChatFontTests
+//
+//  Created by 神州第一坑 on 2019/8/4.
+//  Copyright © 2019 FirstKeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WeChatFontTests : XCTestCase
+
+@end
+
+@implementation WeChatFontTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
